f4f0aa8ca90aa4810a7c6462ec9a1bf0663f2c41
Kechroud <122513464+Shanezdz@users.noreply.github.com>
2026-08-07 19:53:06 +0100

    Add Zaouia logo to site header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,20 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="site-header">
-      <Link className="brand" href="/">ZAOUIA</Link>
+      <Link className="brand brand-with-logo" href="/" aria-label="Zaouia – Accueil">
+        <Image
+          src="/images/logo.png"
+          alt="Logo Zaouia"
+          width={48}
+          height={48}
+          className="header-logo"
+          priority
+        />
+        <span>ZAOUIA</span>
+      </Link>
       <nav>
         <Link href="/#numeros">Numéros</Link>
         <Link href="/#a-propos">À propos</Link>
